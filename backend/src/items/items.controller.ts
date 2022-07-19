e67c511f0608f538a5b5c128be0602f7ddfdc77d
shun-harutaro <shun_harutaro@protonmail.com>
2022-07-19 16:03:03 +0900

    Implement CRUD

--- backend/src/items/items.controller.ts
+++ backend/src/items/items.controller.ts
@@ -1,25 +1,53 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { 
+    Body,
+    Controller, 
+    Get, 
+    Post,
+    Delete, 
+    Param,
+    Patch, 
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './items.model';
 import { itemStatus } from './item-status.enum';
 
 @Controller('items')
 export class ItemsController {
-    constructor(private readonly itemService: ItemsService) {}
+    constructor(private readonly itemsService: ItemsService) {}
 
     @Get()
     findAll() {
-        return this.itemService.findAll();
+        return this.itemsService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id') id: string): Item {
+        return this.itemsService.findById(id);
     }
 
     @Post()
     create(@Body('id') id: string, @Body('body') body: string): Item {
-        return this.itemService.create({
+        console.log(id);
+        const item = {
             id,
             body,
             status: itemStatus.TODO,
             createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-        });
+            updatedAt: new Date().toISOString(), 
+        }
+        return this.itemsService.create(item);
+    }
+
+    @Patch(':id')
+    updateToDoStatus(
+        @Param('id') id: string,
+        @Body('status') status: itemStatus,
+    ): Item {
+        return this.itemsService.updateStatus(id,status);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): string {
+        return this.itemsService.delete(id);
     }
 }
--- backend/src/items/items.service.ts
+++ backend/src/items/items.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { itemStatus } from './item-status.enum';
 import { Item } from './items.model';
 
 @Injectable()
 export class ItemsService {
     private todoItems: Item[] = [];
 
-    findAll() {
+    findAll(): Item[] {
         return this.todoItems;
     }
 
-    create(item: Item) {
+    findById(id: string): Item {
+        return this.todoItems.find((item) => item.id === id);
+    }
+
+    create(item: Item): Item {
         this.todoItems.push(item);
         return item;
+    }
+
+    updateStatus(id: string, status: itemStatus): Item {
+        const targetItem = this.findById(id);
+        targetItem.status = status;
+        return targetItem;
+    }
+
+    delete(id: string): string {
+        this.todoItems = this.todoItems.filter((item) => item.id !== id);
+        return `item_id: ${id} delete success`;
     }
 }
